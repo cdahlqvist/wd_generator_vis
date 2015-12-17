0b930a6fa8a9632124e77de29a646482e3b25817
Christian Dahlqvist <christian@elastic.com>
2015-12-17 14:56:09 +0000

    WIP

--- public/wd_generator_vis_controller.js
+++ public/wd_generator_vis_controller.js
@@ -4,18 +4,18 @@ define(function (require) {
     $scope.vis.params.wd_generator.exploit_result = false;
     $scope.vis.params.wd_generator.server_failure_result = false;
     $scope.vis.params.wd_generator.dead_link_result = false;
+
+    $scope.simulate = function(value) {
+      var $req = $scope.vis.params.wd_generator.url + "/" + value;
+      var $resp_var = value + "_result";
+
+      $http.get($req).then(function(response) {
+        if(response.result == 'ok') {
+          $scope.vis.params.wd_generator[$resp_var] = 'Success';
+        } else {
+          $scope.vis.params.wd_generator[$resp_var] = 'Failure';
+        }
+      });
+    };
   });
-
-  $scope.simulate = function(value) {
-    var $req = $scope.vis.params.wd_generator.url + "/" + value;
-    var $resp_var = value + "_result";
-
-    $http.get($req).then(function(response) {
-      if(response.result == 'ok') {
-        $scope.vis.params.wd_generator[$resp_var] = 'Success';
-      } else {
-        $scope.vis.params.wd_generator[$resp_var] = 'Failure';
-      }
-    });
-  };
 });
